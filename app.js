a58564fa8a8ac84336f30275d0e4a38d4284a095
shinyi <shinyic.design@gmail.com>
2019-07-27 17:08:55 -0400

    fixed text field

--- app.js
+++ app.js
@@ -37,11 +37,7 @@
                  // console.log(currentToDoList, index);
                  // console.log(this);
         
-                if(currentToDo === buttonLiText){
-                // remove from array
-                 toDos.splice(index, 1);
-                }
-                console.log(toDos);
+           
          });
     })
 
@@ -55,7 +51,7 @@
         	toDoList.appendChild(newLi);
         	//empty the input
         	newToDoText.value = "";
-                newLi.appendChild(mdl-checkbox__input)
+
 
         });
 };
